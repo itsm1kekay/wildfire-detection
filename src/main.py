8acfcf1c02626f38cbc7c9424e300bec51e3ca62
Mukul Narwani <narwanimukul103@gmail.com>
2025-03-09 22:05:27 +0000

    ADD working UNET and model

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import zarr
-import fsspec
-import xarray as xr
-
-
-url = 'https://storage.de.cloud.ovh.net/v1/AUTH_84d6da8e37fe4bb5aea18902da8c1170/uc3/uc3cube.zarr'
-ds = xr.open_zarr(fsspec.get_mapper(url), consolidated=True)
-print(ds.head())
